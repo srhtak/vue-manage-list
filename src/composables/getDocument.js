9c7436a7b9d3c9776b5a2beee6f0b122d9c105d3
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-06-11 20:50:35 +0300

    Playlist template added

--- src/composables/getDocument.js
+++ src/composables/getDocument.js
@@ -1,7 +1,7 @@
 import { ref, watchEffect } from "vue";
 import { projectFirestore } from "../firebase/config";
 
-const getDocuments = (collection, id) => {
+const getDocument = (collection, id) => {
   const document = ref(null);
   const error = ref(null);
 
@@ -30,4 +30,4 @@ const getDocuments = (collection, id) => {
   return { error, document };
 };
 
-export default getDocuments;
+export default getDocument;
--- src/views/playlist/ListDetail.vue
+++ src/views/playlist/ListDetail.vue
@@ -1,11 +1,67 @@
 <template>
-  <div>
-    <h1>Serhat Detail {{ id }}</h1>
+  <div class="error" v-if="error">{{ error }}</div>
+  <div v-if="playlist" class="playlist-details m-8">
+    <!-- playlist information -->
+    <div class="playlist-info">
+      <div class="cover">
+        <img :src="playlist.coverUrl" />
+      </div>
+      <h2 class="text-2xl text-blue-500">{{ playlist.title }}</h2>
+      <p class="username">Created by {{ playlist.userName }}</p>
+      <p class="description">{{ playlist.description }}</p>
+    </div>
+
+    <!-- song list -->
+    <div class="song-list">
+      <p>song list here</p>
+    </div>
   </div>
 </template>
 
 <script setup>
+import getDocument from "@/composables/getDocument";
+import Spinner from "@/components/Spinner.vue";
+
 const props = defineProps(["id"]);
+
+const { error, document: playlist } = getDocument("playlist", props.id);
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+.playlist-details {
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  gap: 80px;
+}
+.cover {
+  overflow: hidden;
+  border-radius: 14px;
+  position: relative;
+  padding: 160px;
+}
+.cover img {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-width: 100%;
+  min-height: 100%;
+  max-width: 200%;
+  max-height: 200%;
+}
+.playlist-info {
+  text-align: center;
+}
+.playlist-info h2 {
+  text-transform: capitalize;
+  font-size: 28px;
+  margin-top: 20px;
+}
+.playlist-info p {
+  margin-bottom: 20px;
+}
+.username {
+  color: #000;
+  font-weight: 700;
+}
+</style>
