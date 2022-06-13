a50cb2b59385cfbd90bab500a5a1767a93e7a461
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-06-14 00:27:56 +0300

    userList page modified

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div v-if="formattedList" class="grid grid-cols-3 gap-10 p-8">
+  <div
+    v-if="formattedList"
+    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 p-8"
+  >
     <ListView :playlists="formattedList" />
   </div>
   <div v-else>
--- src/views/playlist/UserList.vue
+++ src/views/playlist/UserList.vue
@@ -2,6 +2,7 @@
 import getCollection from "@/composables/getCollection";
 import getUser from "@/composables/getUser";
 import ListView from "@/components/ListView.vue";
+import Spinner from "@/components/Spinner.vue";
 
 const { user } = getUser();
 const { documents: playlists } = getCollection("playlists", [
@@ -13,8 +14,21 @@ const { documents: playlists } = getCollection("playlists", [
 
 <template>
   <div>
-    <h1>My Playlist</h1>
-    <ListView :playlists="playlists" />
+    <h1 class="text-2xl font-bold px-6">My Playlist</h1>
+    <div
+      v-if="playlists"
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 p-8"
+    >
+      <ListView :playlists="playlists" />
+    </div>
+    <div v-else>
+      <Spinner />
+    </div>
+    <router-link
+      class="bg-blue-400 text-white px-8 py-3 mx-8 rounded-sm shadow-lg hover:bg-blue-500"
+      :to="{ name: 'CreatePlayList' }"
+      >Create a new list</router-link
+    >
   </div>
 </template>
 
